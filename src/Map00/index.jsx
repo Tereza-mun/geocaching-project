99ab2941f67e8c0327176c1c9cbc7e6d85256068
Monika Kalajtzidu <m.kalajtzidu@gmail.com>
2021-06-04 13:39:30 +0200

    pinpoints.js separate const file with long/lat

--- src/Map00/index.jsx
+++ src/Map00/index.jsx
@@ -10,67 +10,9 @@ import spendlikUrl from "../img/spendlik.svg";
 import infoUrl from "../img/magnifier.svg";
 import "./marker.css";
 import "./zoom.css";
+import { pinpoints } from "./pinpoints.js";
 
 const Map00 = () => {
-  const pinpoints = [
-    {
-      // PLANETARIUM - GIORDANO BRUNO
-      id: 1,
-      ikonaUrl: infoUrl,
-      latitude: 50.105291,
-      longitude: 14.427648,
-    },
-    {
-      // RYBNIK - DUCK HEADS alebo ROSTLINKY TABULA
-      id: 2,
-      ikonaUrl: infoUrl,
-      latitude: 50.1070722,
-      longitude: 14.4221119,
-    },
-    {
-      // CITARNA
-      id: 3,
-      ikonaUrl: infoUrl,
-      latitude: 50.1076472,
-      longitude: 14.4176525,
-    },
-    {
-      // STARA FONTANA + JAPANESE TREE
-      id: 4,
-      ikonaUrl: infoUrl,
-      latitude: 50.1058994,
-      longitude: 14.4140292,
-    },
-    {
-      // STUDNICNI DOMEK
-      id: 5,
-      ikonaUrl: infoUrl,
-      latitude: 50.1049194,
-      longitude: 14.4083556,
-    },
-    {
-      // PAMATNIK MECSEROVY SILNICE
-      id: 6,
-      ikonaUrl: infoUrl,
-      latitude: 50.1044306,
-      longitude: 14.4150547,
-    },
-    {
-      // OSTROVCEK - JEZERO SLUNECNICE
-      id: 7,
-      ikonaUrl: infoUrl,
-      latitude: 50.1062383,
-      longitude: 14.4166774,
-    },
-    {
-      // SLECHTOVA RESTAURACE - 30 HLAV
-      id: 8,
-      ikonaUrl: infoUrl,
-      latitude: 50.1048456,
-      longitude: 14.417837,
-    },
-  ];
-
   const [viewport, setViewport] = useState({
     latitude: 50.1045369,
     longitude: 14.4310347,
