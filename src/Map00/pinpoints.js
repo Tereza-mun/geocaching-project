24057c849867ffaac21149f853451c1c0950eccc
Monika Kalajtzidu <m.kalajtzidu@gmail.com>
2021-06-06 19:30:47 +0200

    comment out questions

--- src/Map00/pinpoints.js
+++ src/Map00/pinpoints.js
@@ -1,119 +1,119 @@
 import infoUrl from "../img/magnifier.svg";
 
 export const pinpoints = [
-  // {
-  //   // START POINT
-  //   id: 0,
-  //   ikonaUrl: infoUrl,
-  //   latitude: 50.1045369,
-  //   longitude: 14.4310347,
-  //   questionText: "How many columns does the fountain have?",
-  //   answerOptions: [
-  //     { answerText: 10, isCorrect: false },
-  //     { answerText: 13, isCorrect: false },
-  //     { answerText: 14, isCorrect: false },
-  //     { answerText: 12, isCorrect: true },
-  //   ],
-  // },
-  // {
-  //   // PLANETARIUM - GIORDANO BRUNO
-  //   id: 1,
-  //   ikonaUrl: infoUrl,
-  //   latitude: 50.105291,
-  //   longitude: 14.427648,
-  //   questionText:
-  //     "What is the name of the philosopher and astronomer on the plaque of the planetarium?",
-  //   answerOptions: [
-  //     { answerText: "Aristoteles", isCorrect: false },
-  //     { answerText: "Buzz Aldrin", isCorrect: false },
-  //     { answerText: "Giordano Bruno", isCorrect: true },
-  //     { answerText: "Neil Armstrong", isCorrect: false },
-  //   ],
-  // },
-  // {
-  //   // RYBNIK - DUCK HEADS alebo ROSTLINKY TABULA
-  //   id: 2,
-  //   ikonaUrl: infoUrl,
-  //   latitude: 50.1070722,
-  //   longitude: 14.4221119,
-  //   questionText: "What animal does the wooden pier represent?",
-  //   answerOptions: [
-  //     { answerText: "Duck", isCorrect: true },
-  //     { answerText: "Swan", isCorrect: false },
-  //     { answerText: "Frog", isCorrect: false },
-  //     { answerText: "Goose", isCorrect: false },
-  //   ],
-  // },
-  // {
-  //   // CITARNA
-  //   id: 3,
-  //   ikonaUrl: infoUrl,
-  //   latitude: 50.1076472,
-  //   longitude: 14.4176525,
-  //   questionText: "What is the location which you just reached used for?",
-  //   answerOptions: [
-  //     { answerText: "Order a coffee", isCorrect: false },
-  //     { answerText: "Borrow a book", isCorrect: true },
-  //     { answerText: "Restroom", isCorrect: false },
-  //     { answerText: "Rent a bike", isCorrect: false },
-  //   ],
-  // },
-  // {
-  //   // STARA FONTANA + JAPANESE TREE
-  //   id: 4,
-  //   ikonaUrl: infoUrl,
-  //   latitude: 50.1058994,
-  //   longitude: 14.4140292,
-  //   questionText: "What is the attribute of the tree closes to the fountain?",
-  //   answerOptions: [
-  //     { answerText: "Chinese", isCorrect: false },
-  //     { answerText: "Philippines", isCorrect: false },
-  //     { answerText: "Australian", isCorrect: false },
-  //     { answerText: "Japanese", isCorrect: true },
-  //   ],
-  // },
-  // {
-  //   // STUDNICNI DOMEK
-  //   id: 5,
-  //   ikonaUrl: infoUrl,
-  //   latitude: 50.1049194,
-  //   longitude: 14.4083556,
-  //   questionText: "What is written on the 3rd line on the plaque?",
-  //   answerOptions: [
-  //     { answerText: "MDCCCIV", isCorrect: false },
-  //     { answerText: "MDCCCII", isCorrect: true },
-  //     { answerText: "MDCCCVI", isCorrect: false },
-  //     { answerText: "MDCCCX", isCorrect: false },
-  //   ],
-  // },
-  // {
-  //   // PAMATNIK MECSEROVY SILNICE
-  //   id: 6,
-  //   ikonaUrl: infoUrl,
-  //   latitude: 50.1044306,
-  //   longitude: 14.4150547,
-  //   questionText: "What year is stated on the stone monument?",
-  //   answerOptions: [
-  //     { answerText: 1869, isCorrect: false },
-  //     { answerText: 1888, isCorrect: false },
-  //     { answerText: 1861, isCorrect: true },
-  //     { answerText: 1851, isCorrect: false },
-  //   ],
-  // },
-  // {
-  //   // OSTROVCEK - JEZERO SLUNECNICE
-  //   id: 7,
-  //   ikonaUrl: infoUrl,
-  //   latitude: 50.1062383,
-  //   longitude: 14.4166774,
-  //   questionText: "What is the name of the pond surrounding the island?",
-  //   answerOptions: [
-  //     { answerText: "Šestáček", isCorrect: false },
-  //     { answerText: "Rozínek", isCorrect: false },
-  //     { answerText: "Srpeček", isCorrect: false },
-  //     { answerText: "Slunečnice", isCorrect: true },
-  //   ],
-  // },
+  {
+    // START POINT
+    id: 0,
+    ikonaUrl: infoUrl,
+    latitude: 50.1045369,
+    longitude: 14.4310347,
+    questionText: "How many columns does the fountain have?",
+    answerOptions: [
+      { answerText: 10, isCorrect: false },
+      { answerText: 13, isCorrect: false },
+      { answerText: 14, isCorrect: false },
+      { answerText: 12, isCorrect: true },
+    ],
+  },
+  {
+    // PLANETARIUM - GIORDANO BRUNO
+    id: 1,
+    ikonaUrl: infoUrl,
+    latitude: 50.105291,
+    longitude: 14.427648,
+    questionText:
+      "What is the name of the philosopher and astronomer on the plaque of the planetarium?",
+    answerOptions: [
+      { answerText: "Aristoteles", isCorrect: false },
+      { answerText: "Buzz Aldrin", isCorrect: false },
+      { answerText: "Giordano Bruno", isCorrect: true },
+      { answerText: "Neil Armstrong", isCorrect: false },
+    ],
+  },
+  {
+    // RYBNIK - DUCK HEADS alebo ROSTLINKY TABULA
+    id: 2,
+    ikonaUrl: infoUrl,
+    latitude: 50.1070722,
+    longitude: 14.4221119,
+    questionText: "What animal does the wooden pier represent?",
+    answerOptions: [
+      { answerText: "Duck", isCorrect: true },
+      { answerText: "Swan", isCorrect: false },
+      { answerText: "Frog", isCorrect: false },
+      { answerText: "Goose", isCorrect: false },
+    ],
+  },
+  {
+    // CITARNA
+    id: 3,
+    ikonaUrl: infoUrl,
+    latitude: 50.1076472,
+    longitude: 14.4176525,
+    questionText: "What is the location which you just reached used for?",
+    answerOptions: [
+      { answerText: "Order a coffee", isCorrect: false },
+      { answerText: "Borrow a book", isCorrect: true },
+      { answerText: "Restroom", isCorrect: false },
+      { answerText: "Rent a bike", isCorrect: false },
+    ],
+  },
+  {
+    // STARA FONTANA + JAPANESE TREE
+    id: 4,
+    ikonaUrl: infoUrl,
+    latitude: 50.1058994,
+    longitude: 14.4140292,
+    questionText: "What is the attribute of the tree closes to the fountain?",
+    answerOptions: [
+      { answerText: "Chinese", isCorrect: false },
+      { answerText: "Philippines", isCorrect: false },
+      { answerText: "Australian", isCorrect: false },
+      { answerText: "Japanese", isCorrect: true },
+    ],
+  },
+  {
+    // STUDNICNI DOMEK
+    id: 5,
+    ikonaUrl: infoUrl,
+    latitude: 50.1049194,
+    longitude: 14.4083556,
+    questionText: "What is written on the 3rd line on the plaque?",
+    answerOptions: [
+      { answerText: "MDCCCIV", isCorrect: false },
+      { answerText: "MDCCCII", isCorrect: true },
+      { answerText: "MDCCCVI", isCorrect: false },
+      { answerText: "MDCCCX", isCorrect: false },
+    ],
+  },
+  {
+    // PAMATNIK MECSEROVY SILNICE
+    id: 6,
+    ikonaUrl: infoUrl,
+    latitude: 50.1044306,
+    longitude: 14.4150547,
+    questionText: "What year is stated on the stone monument?",
+    answerOptions: [
+      { answerText: 1869, isCorrect: false },
+      { answerText: 1888, isCorrect: false },
+      { answerText: 1861, isCorrect: true },
+      { answerText: 1851, isCorrect: false },
+    ],
+  },
+  {
+    // OSTROVCEK - JEZERO SLUNECNICE
+    id: 7,
+    ikonaUrl: infoUrl,
+    latitude: 50.1062383,
+    longitude: 14.4166774,
+    questionText: "What is the name of the pond surrounding the island?",
+    answerOptions: [
+      { answerText: "Šestáček", isCorrect: false },
+      { answerText: "Rozínek", isCorrect: false },
+      { answerText: "Srpeček", isCorrect: false },
+      { answerText: "Slunečnice", isCorrect: true },
+    ],
+  },
   {
     // SLECHTOVA RESTAURACE - 30 HLAV
     id: 8,
