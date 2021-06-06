12a9167e456d2061d3590bab31e9f34dbf3cf9a2
Tereza Mündleinová <tereza.mundleinova@gmail.com>
2021-06-06 19:10:14 +0200

    Congrat adjusted, points reset

--- src/Congrat/index.jsx
+++ src/Congrat/index.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import logo from "../img/trophy.png";
 import "./style.css";
 
-const Congrat = ({ usernameW, scoreCounter }) => {
+const Congrat = ({ usernameW, scoreCounter, onLeave }) => {
+  useEffect(() => {
+    return () => {
+      console.log("opoustim");
+      onLeave();
+    };
+  }, []);
+
   return (
     <>
-      <h2 className="heading">Congratulation {usernameW}!</h2>
+      <h2 className="heading">Congratulations {usernameW}!</h2>
       <img className="logo-congrat" src={logo}></img>
 
       <button className="btn-points">{scoreCounter} POINTS</button>
